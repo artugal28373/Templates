fe62271483b6ad12707980ceb5ad92d6b9833386
artugal28373 <58047037+artugal28373@users.noreply.github.com>
2023-05-28 23:54:14 +0600

    Create CustomHash.cpp

---
+++ CustomHsh.cpp
@@ -0,0 +1,22 @@
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+const int RANDOM = chrono::high_resolution_clock::now().time_since_epoch().count();
+struct chash {
+    const int RANDOM = (long long)(make_unique<char>().get()) ^ chrono::high_resolution_clock::now().time_since_epoch().count();
+    static unsigned long long hash_f(unsigned long long x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    static unsigned hash_combine(unsigned a, unsigned b) {
+        return a * 31 + b;
+    }
+    int operator()(int x) const {
+        return hash_f(x)^RANDOM;
+    }
+};
+
+gp_hash_table<int, int, chash> mp[200005];
