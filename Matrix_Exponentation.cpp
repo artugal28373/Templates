f82db21a285e851f0fa49c838840fab75783487b
artugal28373 <58047037+artugal28373@users.noreply.github.com>
2023-08-09 22:12:42 +0600

    Update Matrix_Exponentation.cpp

--- Matrix_Exponentation.cpp
+++ Matrix_Exponentation.cpp
@@ -277,3 +277,58 @@ void print(int b[N][N], int n, int m) {
         cout <<endl;
     }
 }
+
+////////////////////
+ 
+const int MOD = 1e9 + 7;
+ 
+struct Matrix {
+ 
+        int n, m;
+        vector < vector < int > > mat;
+ 
+        Matrix(vector < vector < int > > M) : mat(M), n(M.size()), m(M[0].size()) {}
+ 
+        static Matrix id(int n) {
+                vector < vector < int > > I(n, vector <int> (n, 0));
+                for(int i = 0; i < n; i++)
+                        I[i][i] = 1;
+                return Matrix(I);
+        }
+ 
+        Matrix operator * (Matrix A) {
+                int N = n;
+                int M = A.m;
+                int O = m;
+                vector < vector < int > > Temp(N, vector <int> (M, 0));
+ 
+                for(int i = 0; i < N; i++)
+                        for(int j = 0; j < M; j++)
+                                for(int k = 0; k < O; k++)
+                                        Temp[i][j] = ((Temp[i][j] + mat[i][k] * A.mat[k][j]) % MOD + MOD) % MOD;
+                return Matrix(Temp);
+        }
+ 
+        friend ostream& operator << (ostream &out, Matrix A) ;
+ 
+ 
+};
+ 
+ostream& operator << (ostream &out, Matrix A) {
+        for(int i = 0; i < A.n; i++) {
+                if(i) cout << '\n';
+                for(int j = 0; j < A.m; j++)
+                        cout << A.mat[i][j] << " ";
+        }
+}
+ 
+Matrix BigMod(int n, Matrix M, int N) {
+        Matrix ret = M.id(N);
+        while(n) {
+                if(n & 1)
+                        ret = ret * M;
+                n /= 2;
+                M = M * M;
+        }
+        return ret;
+}
