c0faf1d176553568991916a0b0b8397af3f399e6
artugal28373 <58047037+artugal28373@users.noreply.github.com>
2023-09-16 00:51:13 +0600

    Update Divide&ConquireOptimization.cpp

--- Divide&ConquireOptimization.cpp
+++ Divide&ConquireOptimization.cpp
@@ -1,4 +1,4 @@
-
+// https://codeforces.com/problemset/problem/321/E
 int b[mxn][mxn], cost[mxn][mxn];
 int newcost[mxn];
 int dp[mxn], cur_dp[mxn];
@@ -51,3 +51,51 @@ void solve() {
     cout << dp[n];
 }
 
+
+
+/////////////////////////////
+// https://codeforces.com/problemset/problem/868/F
+int b[mxn];
+int dp[mxn], cur_dp[mxn];
+int n, k;
+int cnt[mxn];
+int cost = 0, cl, cr;
+void compute(int l, int r, int opl, int opr) {
+    if(l>r) return;
+    int m = (l+r)/2;
+    pair<int, int> best{LLONG_MAX, -1};
+    // for some i < m, optimal point opl -- assume
+    // for some i > m, optimal point opr -- assume
+    // O(mnlg(n))
+    // costFunction O(1) must
+    // n, m are independent, m represent number of segment & n represents number of element are considered
+
+     while(cl<opl) cost -= --cnt[b[cl++]]; // traversing the array in D&C method when each
+     while(cl>opl) cost += cnt[b[--cl]]++; // elements need to be traversed for each range
+     while(cr<m)cost += cnt[b[++cr]]++;
+     while(cr>m) cost -= --cnt[b[cr--]];
+
+    for(int i=opl; i<=min(m, opr); i++, cl++) {
+        best = min(best, {dp[i-1]+cost, i});
+        cnt[b[i]]--;
+        cost-=cnt[b[i]];
+    }
+    cur_dp[m] = best.first;
+    int op = best.second;
+    compute(l, m-1, opl,op);
+    compute(m+1, r, op, opr);
+}
+void solve() {
+    cin >> n >> k;
+    for(int i=1; i<=n; i++) {
+        cin >> b[i];
+    }
+    for(int i=1; i<=n; i++) dp[i] = LLONG_MAX/2;
+    cl = 1, cr = n;
+    for(int i=1; i<=n; i++) cost+=cnt[b[i]]++;
+    for(int i=1; i<=k; i++) {
+        compute(1, n, 1, n);
+        copy(cur_dp, cur_dp+1+n, dp);
+    }
+    cout << dp[n];
+}
