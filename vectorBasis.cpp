7e65c0b1371a7be0e7b4337fc6b0434e2a9f6bb5
artugal28373 <58047037+artugal28373@users.noreply.github.com>
2024-11-19 23:56:06 +0600

    Add files via upload

--- vectorBasis.cpp
+++ vectorBasis.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-struct vectorBasis{
+#define int long long
+struct vectorBasis {
     int dimension;
     vector<int> basis;
     int sz;
-    vectorBasis(int n){
+    vectorBasis(int n) {
         dimension = n;
         sz = 0;
         basis = vector<int>(n, -1);
     }
-   void addNewElement(int msk){
-        for(int i=0; i<dimension; i++){
-            if((msk>>i)&1){
-                if(~basis[i]){
+    void insert(int msk) {
+        for(int i=0; i<dimension; i++) {
+            if((msk>>i)&1) {
+                if(~basis[i]) {
                     msk^=basis[i];
-                }else{
+                } else {
                     basis[i] = msk;
                     sz++;
                     return;
@@ -23,17 +23,33 @@ struct vectorBasis{
             }
         }
     }
-    bool check(int msk){
-       for(int i=0; i<dimension; i++){
-        if((msk>>i)&1){
-            if(~basis[i]) return 0;
-            else msk ^= basis[i];
+    bool find(int msk) {
+        for(int i=0; i<dimension; i++) {
+            if((msk>>i)&1) {
+                if(~basis[i]) return 0;
+                else msk ^= basis[i];
+            }
         }
-       }
-       return 1;
+        return 1;
     }
-    int currentBasisSize(){
+    int getSize() {
         return sz;
     }
 
 };
+
+int32_t main(){
+  int n;
+  cin >>n;
+  vectorBasis vb(64);
+  for(int i=0; i<n; i++){
+    int a;
+    cin >> a;
+    vb.insert(a);
+  }
+  int sz = vb.getSize();
+  int temp = 1;
+  while(sz--) temp *= 2;
+  cout << temp - n;
+
+}
