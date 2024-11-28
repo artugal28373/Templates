03c105683f1fac1d1cb3a48e3bfc7fc34842e47e
artugal28373 <58047037+artugal28373@users.noreply.github.com>
2024-11-28 19:34:12 +0600

    Update InversGraph.cpp

--- InversGraph.cpp
+++ InversGraph.cpp
@@ -1,26 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 // https://judge.yosupo.jp/problem/connected_components_of_complement_graph
-// get TLE for above
 // choose random two nodes and if there are no forbidden and join them using some ds.
 // can choose one that has less number of forbidden edges; 
+
 int32_t main() {
 
     int n;
     cin >> n;
     int m;
     cin >> m;
-    vector<set<int>> g(n);
+    vector<vector<int>> g(n);
     for(int i=0; i<m; i++) {
         int a, b;
         cin >> a>> b;
-//        a--, b--;
-        g[a].insert(b);
-        g[b].insert(a);
+        g[a].push_back(b);
+        g[b].push_back(a);
     }
-    set<int> unused;
+    vector<int> adj(n);
+    vector<int> unused;
     for(int i=0; i<n; i++) {
-        unused.insert(i);
+        unused.push_back(i);
     }
     int c = 0;
     vector<vector<int>> connected;
@@ -28,25 +29,32 @@ int32_t main() {
         c++;
         connected.emplace_back();
         queue<int> q;
-        q.push(*unused.begin());
-        unused.erase(q.front());
-        vector<int> deleted;
+        q.push(unused.back());
+        unused.pop_back();
         while(!q.empty()) {
             int u = q.front();
             connected.back().push_back(u);
+            vector<int>nxt_it;
             q.pop();
+            for(int &x: g[u]) {
+                adj[x]=1;
+            }
             // since forbidden edges are not more than m, it quickly reduces size of unused
-            for(const int &x: unused) {
-                if(g[u].count(x)==0) {
+            for(int &x: unused) {
+                if(adj[x]==0) {
                     q.push(x);
-                    deleted.push_back(x);
+                } else {
+                    nxt_it.push_back(x);
                 }
             }
-            for(int &x: deleted) unused.erase(x);
+            unused = nxt_it;
+            for(int &x: g[u]) {
+                adj[x]=0;
+            }
         }
     }
     cout << c <<"\n";
-    for(vector<int> &vec: connected){
+    for(vector<int> &vec: connected) {
         cout << vec.size() << " ";
         for(int &x: vec) cout << x << " ";
         cout << "\n";
